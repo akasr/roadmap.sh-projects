705133258d2eb6e2aa56eefc246a4b6b48e5c7f2
Akash Raj <ebookexer@gmail.com>
2024-11-14 21:49:03 +0530

    Add command to list all tasks

--- backend/task-tracker/src/command.js
+++ backend/task-tracker/src/command.js
@@ -1,19 +1,41 @@
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import { addTask } from './tasks.js';
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { addTask, listAll } from "./tasks.js";
+
+const listTasks = (tasks) => {
+  tasks.forEach(({ id, description, status, createdAt, updatedAt }) => {
+    console.log(`id: ${id}`);
+    console.log(`description: ${description}`);
+    console.log(`status: ${status}`);
+    console.log(`created at: ${createdAt}`);
+    console.log(`updated at ${updatedAt}`);
+    console.log("\n");
+  });
+};
 
 yargs(hideBin(process.argv))
-    .command('add <task>', 'Add a new task', (yargs) => {
-        return yargs.positional('task', {
-            describe: 'Task to add',
-            type: 'string'
-        })
-    }, async (argv) => {
-        const task = await addTask(argv.task);
-        console.log(`Task added: ${task.description}`);
-    })
-    .command('list', 'List all the tasks', (yargs) => {}, async(argv) => {
-        
-    })
-    .demandCommand(1)
-    .parse();
+  .command(
+    "add <task>",
+    "Add a new task",
+    (yargs) => {
+      return yargs.positional("task", {
+        describe: "Task to add",
+        type: "string",
+      });
+    },
+    async (argv) => {
+      const task = await addTask(argv.task);
+      console.log(`Task added: ${task.description}`);
+    }
+  )
+  .command(
+    "list",
+    "List all the tasks",
+    (yargs) => {},
+    async (argv) => {
+      const tasks = await listAll();
+      listTasks(tasks);
+    }
+  )
+  .demandCommand(1)
+  .parse();
--- backend/task-tracker/src/tasks.js
+++ backend/task-tracker/src/tasks.js
@@ -1,14 +1,19 @@
-import {insertDB, getDB, saveDB} from './db.js';
+import { insertDB, getDB, saveDB } from "./db.js";
 
 export const addTask = async (task) => {
-    const newTask = {
-        id: Date.now(),
-        description: task,
-        status: "in-progress",
-        createdAt: new Date().toISOString(),
-        updatedAt: null
-    }
+  const newTask = {
+    id: Date.now(),
+    description: task,
+    status: "in-progress",
+    createdAt: new Date().toISOString(),
+    updatedAt: "Still to Happen",
+  };
 
-    await insertDB(newTask);
-    return newTask;
-}
+  await insertDB(newTask);
+  return newTask;
+};
+
+export const listAll = async () => {
+  const {tasks} = await getDB();
+  return tasks;
+};
